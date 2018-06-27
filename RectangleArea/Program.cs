30ec441da5d84bc0dc76856aa3f0d8eb75e82757
DerAd <a.derron@wp.pl>
2018-06-27 23:58:45 +0200

    first try

---
+++ RectangleArea/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleArea
+{
+    class Rectangle
+    {
+        double length = Convert.ToDouble(Console.ReadLine());
+        double width = Convert.ToDouble(Console.ReadLine());
+
+        public double GetArea()
+        {
+            return length * width;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+    }
+
+    class ExecuteRectangle
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+            r.Display();
+            Console.ReadLine();
+        }
+    }
+}
